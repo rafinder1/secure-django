7adfe5ee0dc1058bbe49988eab2e0819c37adf6c
rafinder1 <rafpro33@gmail.com>
2025-07-09 12:14:52 -0400

    feat: add new variables to db

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,25 @@ variable "region" {
   description = "Region GCP"
   type        = string
 }
+
+variable "db_user" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "tier" {
+  type    = string
+  default = "db-f1-micro"
+}
+
+variable "instance_name" {
+  default = "app-cloudsql-instance"
+}
