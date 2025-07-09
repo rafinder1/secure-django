88d167892ba4567507ef7cceb08e4d52465a8d37
rafinder1 <rafpro33@gmail.com>
2025-07-09 12:15:32 -0400

    feat: create db and user

---
+++ terraform/cloudsql_postgres.tf
@@ -0,0 +1,34 @@
+resource "google_sql_database_instance" "postgres_instance" {
+  name             = var.instance_name
+  database_version = "POSTGRES_17"
+  region           = var.region
+
+  settings {
+    tier = var.tier
+
+    ip_configuration {
+      ipv4_enabled    = false
+
+    #   private_network = var.private_network # np. "projects/YOUR_PROJECT_ID/global/networks/default"
+    }
+
+    backup_configuration {
+      enabled = true
+    }
+
+    activation_policy = "ALWAYS"
+  }
+
+  deletion_protection = false
+}
+
+resource "google_sql_user" "postgres_user" {
+  name     = var.db_user
+  instance = google_sql_database_instance.postgres_instance.name
+  password = var.db_password
+}
+
+resource "google_sql_database" "postgres_db" {
+  name     = var.db_name
+  instance = google_sql_database_instance.postgres_instance.name
+}
